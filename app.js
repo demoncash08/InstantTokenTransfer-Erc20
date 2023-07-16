1eea6649953f9f02c2d0d83905ad170a83681d89
Mehul <87169681+demoncash08@users.noreply.github.com>
2023-07-16 22:40:20 +0530

    first commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const provider = new ethers.providers.JsonRpcProvider(sepolia)
 
 // You can also use an ENS name for the contract address
 //Token address
-const TokenAddress = '0x6d1e1cf90cb3d89F9839531063D7fb25c50598fC'
+const TokenAddress = '' //Token address here
 
 // The ERC-20 Contract ABI, which is a common contract interface
 // for tokens (this is the Human-Readable ABI format)
@@ -33,22 +33,21 @@ const Balance = async () => {
   console.log('Block num is  ', await provider.getBlockNumber())
 
   const mainWallet = ethers.Wallet.fromMnemonic(
-    'wrong poem sniff engine chief scan erase creek worth chunk plug answer'
+    '' //Wrtie Mnemonic phrase here
   )
-  // 0x52E30F1F1F2A41DbCF0C320FbfaF192fC88C8F80
-  //Above address is the wallet derived from given mnemonic
+
   wallet = mainWallet.connect(provider)
   const TokenContract = new ethers.Contract(TokenAddress, TokenAbi, wallet)
 
   //Balance of the ERC20 Token
-  balance = await TokenContract.balanceOf('0x52E30F1F1F2A41DbCF0C320FbfaF192fC88C8F80')
+  balance = await TokenContract.balanceOf('') //Write token address here
   balance = ethers.utils.formatUnits(balance, 18)
   console.log('Balance is =', balance)
 
   if (balance > 0) {
     try {
       const token = ethers.utils.parseUnits(balance, 18)
-      tx = await TokenContract.transfer('0x3b259A4d129F4Ddbeba62ADDdcdFD5fbc5EF4175', token)
+      tx = await TokenContract.transfer('', token) //Destination ERC20 compatible address where tokens are to be trnasfered
       console.log('Sent')
     } catch (error) {
       console.log(error)
@@ -60,3 +59,4 @@ setInterval(async () => {
   const result = await Balance()
   console.log(balance)
 }, 10000)
+//Time out interval for bot to re run.
